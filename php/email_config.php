e6f3cb47379fa59a1737922bdacc444824e1b997
CleberNandes <stiflerbycleber@gmail.com>
2017-08-28 12:11:20 -0300

    Primeiro commit do meu portfolio

---
+++ php/email_config.php
@@ -0,0 +1,15 @@
+<?php
+	
+	//your email here
+	$to_email = 'williansugiyama@gmail.com';
+	
+	//subject of email you will receive
+	$subject = 'Contato Portfolio';	
+	
+	//Error Messages
+	$invalid_name = 'Por favor insira seu nome';
+	$invalid_email = 'Por favor insira um e-mail valido';
+	$invalid_message = 'Por favor insira sua mensagem';
+	$sending_error = 'Desculpa, não conseguimos enviar seu e-mail';
+
+ ?>
