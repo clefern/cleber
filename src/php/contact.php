1d8bb183cff725dc20cd9e8bf9dd1c525f0b3b08
CleberNandes <cleber_sim@outlook.com>
2020-04-26 22:46:59 -0300

    verificando erro de enviar email

--- src/php/contact.php
+++ src/php/contact.php
@@ -26,21 +26,20 @@
     // email header
 
 	$headers = "From: ".$name." <".$email.">\r\nReply-To: ".$email."";
-	// print_r($error);
-	echo $error; // error message
-	// if (!$error){
+	
+	if (!$error){
 		
-	// 	// sending email
-	// 	$sent = mail($to_email,$subject,$message,$headers); 
+		// sending email
+		$sent = mail($to_email,$subject,$message,$headers); 
 		
-	// 	if ($sent) {
-	// 			// if message sent successfully
-	// 			echo "SEND"; 
-	// 		} else {
-	// 			// error message
-	// 			echo $sending_error; 
-	// 		}
-	// } else {
-	// 	echo $error; // error message
-	// }
+		if ($sent) {
+				// if message sent successfully
+				echo "SEND"; 
+			} else {
+				// error message
+				echo $sending_error; 
+			}
+	} else {
+		echo $error; // error message
+	}
 ?>
